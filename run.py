e4e0a14d2ccb17eb53b5a1f62549e5b7e3725a1d
vladar21 <vladar21@gmail.com>
2023-10-29 15:41:05 +0000

    add display_start_board func

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Start board
+def display_start_board():
+    print("\nWelcome to the good old game of Tic-Tac-Toe in a modern interpretation.\n")
+    print("Your opponent will be a self-learning artificial intelligence.\n")
+    start_board = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+    print(f" {start_board[0]} | {start_board[1]} | {start_board[2]} ")
+    print(" --------- ")
+    print(f" {start_board[3]} | {start_board[4]} | {start_board[5]} ")
+    print(" --------- ")
+    print(f" {start_board[6]} | {start_board[7]} | {start_board[8]} ")
+
+# Display start board
+display_start_board()
