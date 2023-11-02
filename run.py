44b8c78b9b18e72b3b4a27787f9ff60d2235afe5
vladar21 <vladar21@gmail.com>
2023-11-02 01:19:38 +0000

    feat: add update_leaderboards func

--- run.py
+++ run.py
@@ -13,6 +13,8 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('tic_tac_toe')
+leadersboard_sheet = SHEET.worksheet('leadersboard')
+tic_tac_toe_data_sheet = SHEET.worksheet('tic_tac_toe_data_sheet')
 
 # Start board
 def display_start_board():
@@ -28,6 +30,54 @@ def display_start_board():
     print(f" {start_board[3]} | {start_board[4]} | {start_board[5]} ")
     print(" --------- ")
     print(f" {start_board[6]} | {start_board[7]} | {start_board[8]} ")
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+def update_leaderboards(leaderboard_sheet, human_nickname, win_human, win_ai):
+   
+    leaderboard_old_data = leaderboard_sheet.get_all_values()
+
+    # Find the row index for the given human_nickname, if it exists
+    row_index = None
+    for i, row in enumerate(leaderboard_old_data):
+        if row[0] == human_nickname:
+            row_index = i
+            break
+
+    # If the human_nickname exists, update the row; otherwise, add a new row
+    if row_index is not None:
+        # Update win_human, win_ai, and total_games
+        win_human = int(row[2]) + win_human
+        win_ai = int(row[3]) + win_ai
+        total_games = int(row[1]) + 1
+
+        leaderboard_sheet.update(f'C{row_index+2}', [[win_human]])
+        leaderboard_sheet.update(f'D{row_index+2}', [[win_ai]])
+        leaderboard_sheet.update(f'B{row_index+2}', [[total_games]])
+    else:
+        # Create a new row with the given values
+        new_row = [human_nickname, 1, win_human, win_ai]
+        leaderboard_sheet.append_table(new_row)
+
+
+
+    
+
+def save_board_to_google_sheets(board, move):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = Credentials.from_service_account_file('creds.json', scopes=scope)
+    gc = gspread.authorize(creds)
+
+    spreadsheet = gc.open_by_key('1N2eosTHbj1OdRRdJkLrf2IKxIXOp1k80wb2kht6caJc')
+    worksheet = spreadsheet.worksheet('cross_zerro')
+
+    board_str = "".join(["X" if cell == 1 else "O" if cell == -1 else " " for row in board for cell in row])
+    data_to_insert = [[board_str, move]]
+    worksheet.insert_rows(data_to_insert, 2)
 
 def main():
     # Main game loop
