eac94886872ee22e6f0ca3adf7a4b7c5dddbb299
vladar21 <vladar21@gmail.com>
2023-11-05 00:24:55 +0000

    feat: add load model from google drive

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+# work with warnings
+import warnings
+# Suppress all UserWarnings
+warnings.filterwarnings('ignore', category=UserWarning)
+
 # Google Sheet
 import gspread
 from google.oauth2.service_account import Credentials
@@ -7,16 +12,19 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 # my library
-from funcs import display_start_game
+from funcs import display_start_game, display_board, display_leadersboard, update_leadersboard, save_board_to_google_sheets, check_game_status
 # Google Drive
 from googleapiclient.discovery import build
 import io
 from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaIoBaseDownload
 import h5py
-from googleapiclient.http import MediaFileUpload
+from keras.models import load_model
+
 
 # tf.data.experimental.enable_debug_mode()
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 SERVICE_ACCOUNT_FILE = 'creds.json'
 SCOPES = [
@@ -92,48 +100,86 @@ def save_model_to_google_drive(service, model, model_name):
 
     share_file_with_user(service, "1-kaiGuc_BDqXknwHBSH1ZBK_hrc3WBkq", 'vladar21@gmail.com')
 
+def get_model_id_by_name(service, model_name):
+    results = service.files().list(
+        q=f"name='{model_name}'",
+        pageSize=10,
+        fields="files(id, name)"
+    ).execute()
+    items = results.get('files', [])
 
-    
-def load_or_train_model(worksheet):
-    model_directory = 'tic_tac_toe_model'
-    model_file = os.path.join(model_directory, 'tic_tac_toe_model.h5')
-    
-    if os.path.exists(model_file):
-        model = keras.models.load_model(model_file)
-        return model  # Ensure the model is returned
+    if not items:
+        print(f"No files with name {model_name} found.")
+        return None
     else:
-        data = worksheet.get_all_values()
+        # If several files with the same name are found, we return the ID of the first
+        model_id = items[0]['id']
+        print(f"Found model {model_name} with ID: {model_id}")
+        return model_id
 
-        if not data:
-            print("The worksheet is empty. Starting with empty data.")
-            return None  # Explicitly return None
+def download_model_from_google_drive(service, file_id):
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    fh.seek(0)
+    
+    # Load the Keras model from the h5py File object that is created from the buffer
+    with h5py.File(fh, 'r') as h5file:
+        model = tf.keras.models.load_model(h5file)
+    
+    return model
 
-        X_train = []
-        y_train = []
+def load_or_train_model(service, model_name, worksheet):
+    try:
+        file_id = get_model_id_by_name(service, model_name)
+        if file_id == None:
+            return train_model(worksheet)
+        # Trying to download a model from Google Drive
+        model = download_model_from_google_drive(service, file_id)
+        print("Model loaded successfully from Google Drive.")
+    except Exception as e:
+        print(f"An error occurred while loading the model from Google Drive: {e}")
+        # Here's the code to train the model if loading fails...
+        return train_model(worksheet)
+    
+    return model
 
-        for row in data:
-            if len(row) < 2 or not row[0]:
-                continue
+def train_model(worksheet):
+    data = worksheet.get_all_values()
+    if not data:
+        print("The worksheet is empty. Starting with empty data.")
+        return None  # Explicitly return None
 
-            board_state = [1 if cell == 'X' else -1 if cell == 'O' else 0 for cell in row[0]]
-            X_train.append(board_state)
-            move = int(row[1])
-            y_train.append(move)
+    X_train = []
+    y_train = []
 
-        if len(X_train) > 0:
-            model = keras.Sequential([
-                keras.layers.Input(shape=(9,)),
-                keras.layers.Dense(128, activation='relu'),
-                keras.layers.Dense(9, activation='softmax')
-            ])
+    for row in data:
+        if len(row) < 2 or not row[0]:
+            continue
 
-            model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        board_state = [1 if cell == 'X' else -1 if cell == 'O' else 0 for cell in row[0]]
+        X_train.append(board_state)
+        move = int(row[1])
+        y_train.append(move)
 
-            model.fit(X_train, y_train, epochs=50, verbose=0)
-            return model
-        else:
-            print("No training data available. Starting with an untrained model.")
-            return None
+    if len(X_train) > 0:
+        model = keras.Sequential([
+            keras.layers.Input(shape=(9,)),
+            keras.layers.Dense(128, activation='relu'),
+            keras.layers.Dense(9, activation='softmax')
+        ])
+
+        model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+        model.fit(X_train, y_train, epochs=50, verbose=0)
+        return model
+    else:
+        print("No training data available. Starting with an untrained model.")
+        return None
 
 def load_data_from_google_sheets():
 
@@ -183,136 +229,14 @@ def load_data_from_google_sheets():
 
     return leadersboard_data_sheet, tic_tac_toe_data_sheet
     
-def display_leadersboard(leadersboard_data_sheet):
-
-    # Fetching the data from the sheet
-    leadersboard_data = leadersboard_data_sheet.get_all_values()
-    
-    print("\nLeadersboard")
-
-    # Headers for the table
-    headers = ["PP", "Human Nickname", "Total Games", "Win Human", "Win AI", "Draw"]  # Added "Draw" to headers
-
-    # If there are no data rows or only header row, print the headers and return
-    if len(leadersboard_data) <= 1:
-        print(" | ".join(headers))
-        return
-
-    # Determine the maximum width for each column
-    column_lengths = [len(header) for header in headers]
-    for row in leadersboard_data[1:]:  # Skip the header row in the data
-        for i in range(len(row)):
-            column_lengths[i] = max(column_lengths[i], len(row[i]))
-
-    # Create a format string for each row with appropriate spacing
-    row_format = " | ".join(["{:<" + str(length) + "}" for length in column_lengths])
-
-    # Print the formatted header row
-    print(row_format.format(*headers))
-    print("-" * (sum(column_lengths) + 3 * (len(headers) - 1)))  # Print header separator
-
-    # Print the formatted data rows, skipping the first row which is headers
-    for i, row_data in enumerate(leadersboard_data[1:], start=1):  # Start with 1 to skip header row
-        # If the row has less columns than headers, append empty strings
-        row_data += [""] * (len(headers) - len(row_data))
-        print(row_format.format(i, *row_data))  # Use row_data instead of row for clarity
-
-def update_leadersboard(leadersboard_data_sheet, nickname, result):
-
-    # Fetching the current data from the sheet
-    leadersboard_data = leadersboard_data_sheet.get_all_values()
-    headers = leadersboard_data[0]  # Assuming the first row contains headers
-
-    # Find the indexes for the relevant columns
-    nickname_index = headers.index("human_nickname") + 1  # +1 for Google Sheets indexing
-    total_index = headers.index("total_games") + 1
-    win_index = headers.index("win_human") + 1
-    lose_index = headers.index("win_ai") + 1
-    draws_index = headers.index("draws") + 1
-
-    # Find the player in the leaderboard
-    player_row = None
-    for i, row in enumerate(leadersboard_data[1:], start=2):  # Start=2 to account for header row
-        if row[nickname_index - 1] == nickname:  # -1 to convert back from Google Sheets indexing
-            player_row = i
-            break
-
-    if player_row:
-        # Player exists, update their record
-        cell = leadersboard_data_sheet.cell(player_row, total_index)
-        leadersboard_data_sheet.update_cell(player_row, total_index, int(cell.value) + 1)
-        if result == 'Win':
-            cell = leadersboard_data_sheet.cell(player_row, win_index)
-            leadersboard_data_sheet.update_cell(player_row, win_index, int(cell.value) + 1)
-        elif result == 'Lose':
-            cell = leadersboard_data_sheet.cell(player_row, lose_index)
-            leadersboard_data_sheet.update_cell(player_row, lose_index, int(cell.value) + 1)
-        elif result == 'Draw':
-            cell = leadersboard_data_sheet.cell(player_row, draws_index)
-            leadersboard_data_sheet.update_cell(player_row, draws_index, int(cell.value) + 1)
-    else:
-        # Player doesn't exist, append a new row
-        new_row_values = [''] * len(headers)
-        new_row_values[nickname_index - 1] = nickname
-        new_row_values[total_index - 1] = 1
-        new_row_values[win_index - 1] = 1 if result == 'Win' else 0
-        new_row_values[lose_index - 1] = 1 if result == 'Lose' else 0
-        new_row_values[draws_index - 1] = 1 if result == 'Draw' else 0
-        leadersboard_data_sheet.append_row(new_row_values)
-
-def save_board_to_google_sheets(worksheet, board, move):
-    board_str = "".join(["X" if cell == 1 else "O" if cell == -1 else " " for row in board for cell in row])
-    data_to_insert = [[board_str, move]]
-    worksheet.insert_rows(data_to_insert, 2)
-
-def check_game_status(board):
-    # Check for a win for each player
-    for player in [1, -1]:
-        winning_positions = [
-            # Horizontal
-            board[0], board[1], board[2],
-            # Vertical
-            [board[0][0], board[1][0], board[2][0]],
-            [board[0][1], board[1][1], board[2][1]],
-            [board[0][2], board[1][2], board[2][2]],
-            # Diagonals
-            [board[0][0], board[1][1], board[2][2]],
-            [board[0][2], board[1][1], board[2][0]]
-        ]
-        
-        if any(all(pos == player for pos in win_pos) for win_pos in winning_positions):
-            return True, player  # A win is detected and return the player who won
-
-    # Check for a draw (all cells are filled)
-    if all(cell != 0 for row in board for cell in row):
-        return True, 0  # The game is a draw
-    
-    # If no win or draw, the game is not over
-    return False, None
-
-def display_board(board):
-
-    # Get the total number of rows
-    num_rows = len(board)
-
-    # Iterate over each row in the board
-    for i, row in enumerate(board):
-        # Print the row with 'X', 'O', or ' ' depending on the cell value
-        print(" | ".join(["X" if cell == 1 else "O" if cell == -1 else " " for cell in row]))
-        
-        # Print a horizontal line to separate the rows, except for the last row
-        if i < num_rows - 1:
-            print("-" * 9)
-    print()  # Print a newline at the end for better formatting
-    
-def game(leadersboard_data_sheet, tic_tac_toe_data_sheet, nickname):
+def game(service, model_name, leadersboard_data_sheet, tic_tac_toe_data_sheet, nickname):
     print('\nGame starting.\n')
     display_start_game()
     current_player = 1
     X_train = []
     y_train = []
     board = [[0, 0, 0] for _ in range(3)]  # Initialize the board.
-    model = load_or_train_model(tic_tac_toe_data_sheet)
+    model = load_or_train_model(service, model_name, tic_tac_toe_data_sheet)
     while True:           
         game_over, winner = check_game_status(board)
         if game_over:
@@ -380,13 +304,13 @@ def main():
     start = start.lower()
     # Main game loop
     if start == 'y':
-        game(leadersboard_data_sheet, tic_tac_toe_data_sheet, nickname)
+        game(SERVICE, MODEL_NAME, leadersboard_data_sheet, tic_tac_toe_data_sheet, nickname)
     elif start == 'l':
         display_leadersboard(leadersboard_data_sheet)
         start = str(input("Do you want to play game or exit? \n(Y - game, any other - exit): "))
         start = start.lower()
         if start == 'y':
-            game(leadersboard_data_sheet, tic_tac_toe_data_sheet, nickname)
+            game(SERVICE, MODEL_NAME, leadersboard_data_sheet, tic_tac_toe_data_sheet, nickname)
         else:
             print("\nGame over.\n")
     else:
